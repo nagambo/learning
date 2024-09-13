602c1d010ab97bada465a7839a863c4641f5a9ef
m_labran <eurnag@gmail.com>
2024-09-13 18:40:41 +0400

    lerning commit

---
+++ employee.py
@@ -0,0 +1,25 @@
+
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@comapny.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+
+
+
+emp_1 = Employee('Madu', 'labs', 50000)
+emp_2 = Employee('Test', 'Name', 60000)
+
+
+print(emp_1.email)
+print(Employee.fullname(emp_1))
+
+print(emp_2.email)
+print(Employee.fullname(emp_2))
